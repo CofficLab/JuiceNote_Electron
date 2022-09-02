c4df909fd06fca6d44aa5e51ba9e0f1cfd924a0c
nookery <nooks@qq.com>
2022-09-02 18:25:59 +0800

    Update Navigator.vue

--- src/views/Navigator.vue
+++ src/views/Navigator.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <div class="btn-group flex justify-center mt-2">
-            <router-link class="btn" to="/node">Node</router-link>
-            <router-link class="btn" to="/vue">Vue</router-link>
+            <router-link class="btn" to="/node" active-class="btn-active">Node</router-link>
+            <router-link class="btn" to="/vue" active-class="btn-active">Vue</router-link>
         </div>
     </div>
 </template>
