790ef75a0046596dee0edeb068a2bf6f4d7b8e1f
nookery <nooks@qq.com>
2022-09-02 17:42:33 +0800

    增加藏书

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,34 @@ import Nodejs from './views/Nodejs.vue'
 import Sidebar from './components/Sidebar.vue'
 import VueJs from './views/VueJs.vue'
 import Process from './components/Process.vue'
+import Books from './views/Books.vue'
 
 </script>
 
 <template>
-	<div class="my-8 justify-center flex">
-		<Process></Process>
-	</div>
+	<div>
+		<div class="py-8 justify-center flex flex-col bg-gradient-to-r from-sky-400 to-cyan-800/80">
+			<Process></Process>
 
-	<div class="bg-base-100 min-h-screen min-w-full  flex flex-row gap-4">
-		<div>
-			<Sidebar></Sidebar>
+			<div class="btn-group flex justify-center mt-2">
+				<button class="btn">1</button>
+				<button class="btn btn-active">2</button>
+				<button class="btn">3</button>
+				<button class="btn">4</button>
+			</div>
 		</div>
-		<main class="container mx-auto bg-gradient-to-b from-cyan-700/30 to-blue-900/20 shadow-2xl p-4 rounded-2xl">
-			<div>
-				<Nodejs></Nodejs>
-				<VueJs></VueJs>
+
+		<main class="bg-gradient-to-b from-cyan-700/30 to-blue-900/20 shadow-2xl p-4">
+			<div class="container">
+				<div class="flex justify-center">
+					<!-- <Nodejs></Nodejs> -->
+					<VueJs></VueJs>
+				</div>
 			</div>
 		</main>
+
+		<div>
+			<Books></Books>
+		</div>
 	</div>
 </template>
---
+++ src/views/Books.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="flex flex-row  mt-8">
+        <div class="w-48 bg-gray-300 shadow-2xl shadow-gray-200 mr-2">
+            藏书
+        </div>
+        <div class="bg-white grid grid-cols-3 gap-4 p-8">
+            <div class="card card-side bg-base-100 shadow-xl">
+                <figure><img src="../assets/book.jpeg" alt="Movie"></figure>
+                <div class="card-body">
+                    <h2 class="card-title">New movie is released!</h2>
+                    <p>Click the button to watch on Jetflix app.</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Watch</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card card-side bg-base-100 shadow-xl">
+                <figure><img src="../assets/book.jpeg" alt="Movie"></figure>
+                <div class="card-body">
+                    <h2 class="card-title">New movie is released!</h2>
+                    <p>Click the button to watch on Jetflix app.</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Watch</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card card-side bg-base-100 shadow-xl">
+                <figure><img src="../assets/book.jpeg" alt="Movie"></figure>
+                <div class="card-body">
+                    <h2 class="card-title">New movie is released!</h2>
+                    <p>Click the button to watch on Jetflix app.</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Watch</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card card-side bg-base-100 shadow-xl">
+                <figure><img src="../assets/book.jpeg" alt="Movie"></figure>
+                <div class="card-body">
+                    <h2 class="card-title">New movie is released!</h2>
+                    <p>Click the button to watch on Jetflix app.</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Watch</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
