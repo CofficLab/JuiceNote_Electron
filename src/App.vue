d1c46a3cd4b73a57584ea18822ae4506562d09d8
nooks <nooks@qq.com>
2022-09-03 13:42:23 +0800

    删除多余内容

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import Process from './components/Process.vue'
 	<div>
 		<Process></Process>
 
-		<main class="bg-gradient-to-b from-cyan-700/30 to-blue-900/20 shadow-2xl p-4">
+		<main class="bg-gradient-to-b from-cyan-700/30 to-blue-900/20 shadow-2xl p-4 min-h-screen">
 			<div class="container">
 				<div class="flex justify-center">
 					<router-view></router-view>
--- src/components/Sidebar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="bg-secondary">
-        <ul class="menu bg-base-100 w-56">
-            <li><a>开天辟地</a></li>
-            <li><a class="active">Vue.js</a></li>
-            <li><a>Node.js</a></li>
-        </ul>
-    </div>
-</template>
