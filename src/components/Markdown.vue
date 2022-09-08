2873cb7a2076860df3ad0f03824857fe0bc56443
nookery <nooks@qq.com>
2022-09-08 18:33:23 +0800

    Update Markdown.vue

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -9,8 +9,21 @@
 			<div class="prose min-w-max mx-auto min-h-screen pb-96 container" v-html="html"></div>
 		</div>
 
-		<div class="w-48 prose card bg-base-100 rounded-none shadow-3xl bg-opacity-70 mt-12">
-			<div v-html="toc" class="w-full min-h-screen flex justify-center"></div>
+		<div class="w-48 prose card bg-base-100 rounded-none shadow-3xl bg-opacity-70">
+			<div class="dropdown w-56 fixed z-50">
+				<label tabindex="0" class="btn m-0 w-full rounded-none">
+					<span>xxxxx</span>
+					<svg class="fill-current ml-2" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+						viewBox="0 0 24 24">
+						<path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+					</svg>
+				</label>
+				<ul tabindex="0" class="dropdown-content menu p-2 shadow-2xl bg-blue-200 w-full z-50">
+					<li><a href="">1</a></li>
+					<li><a href="">2</a></li>
+				</ul>
+			</div>
+			<div v-html="toc" class="w-full min-h-screen flex justify-center mt-12"></div>
 		</div>
 	</div>
 </template>
