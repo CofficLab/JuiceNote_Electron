7ec84bd0200faf628514e47931b546e83c2f48cc
nookery <nooks@qq.com>
2022-09-08 18:30:07 +0800

    Update Markdown.vue

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -1,11 +1,15 @@
 <template>
-	<div class="w-full flex flex-row justify-start gap-24 px-0">
-		<div class="mr-48 w-full">
-			<div v-on:dblclick="showEditor" class="h-12"></div>
+	<div class="w-full flex flex-row justify-start gap-0 px-0">
+		<div class="mr-0 w-full">
+			<div class="w-full bg-red-800 rounded-none shadow-3xl bg-opacity-70 h-12 p-0 sticky top-8 z-50">
+				<div class="h-full w-full flex justify-center">
+					<h1 class="align-middle my-0 py-auto place-self-center text-3xl" v-on:dblclick="showEditor">标题</h1>
+				</div>
+			</div>
 			<div class="prose min-w-max mx-auto min-h-screen pb-96 container" v-html="html"></div>
 		</div>
 
-		<div class="w-48 prose card bg-base-100 rounded-none shadow-3xl bg-opacity-70">
+		<div class="w-48 prose card bg-base-100 rounded-none shadow-3xl bg-opacity-70 mt-12">
 			<div v-html="toc" class="w-full min-h-screen flex justify-center"></div>
 		</div>
 	</div>
