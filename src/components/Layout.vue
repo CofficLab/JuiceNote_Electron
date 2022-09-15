f923bf9a6c2fb97c7caa9888040f046c2cacddbc
nookery <nooks@qq.com>
2022-09-15 17:33:08 +0800

    Update Layout.vue

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -2,7 +2,7 @@
   <div class="h-8 bg-stone-900 fixed top-0 z-50 w-full" id="title-bar" v-show="!hideTitleBar"></div>
 
   <div class="h-12 z-50" v-bind:class="hideTitleBar ? 'mt-0' : 'mt-8'">
-    <div class="w-full flex flex-row fixed bg-red-100 z-50">
+    <div class="w-full flex flex-row fixed z-50">
       <Books></Books>
       <div class="flex-grow h-12 bg-base-300 flex justify-center shadow-2xl">
         <div class="place-self-center">
@@ -12,9 +12,9 @@
     </div>
   </div>
 
-  <main class="bg-red-700/10 flex justify-between z-10">
+  <main class="bg-blue-500/40 flex justify-between z-10">
     <div class="w-56 z-10" v-html="toc"></div>
-    <div class="bg-blue-500/40 w-full flex flex-grow mx-auto">
+    <div class="w-full flex flex-grow mx-auto">
       <router-view></router-view>
     </div>
   </main>
