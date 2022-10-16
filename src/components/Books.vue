1daaa8f2d390eded21d3b9d751603e3a698b7da6
nooks <nooks@qq.com>
2022-10-16 22:04:38 +0800

    Update Books.vue

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -2,14 +2,15 @@
   <div class="dropdown z-50 w-full">
     <label tabindex="0" class="btn m-0 w-full rounded-none">
       <span>{{ book ? book.title : "" }}</span>
-      <chevron-down></chevron-down>
+      <!-- <chevron-down></chevron-down> -->
     </label>
     <ul
       tabindex="0"
       class="dropdown-content menu shadow-2xl bg-base-300 border border-primary dark:bg-info-content w-full h-96 overflow-scroll"
     >
-      <li v-for="book in books">
-        <router-link v-bind:to="book.link" v-text="book.id" active-class="active"> </router-link>
+      <li v-for="book in books" class="flex justify-center">
+        <router-link v-bind:to="book.link" v-text="book.id" active-class="active" class="flex justify-center">
+        </router-link>
       </li>
     </ul>
   </div>
