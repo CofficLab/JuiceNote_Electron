14b42d4b10805163d8d114e0c6b30aca3836e923
nooks <nooks@qq.com>
2022-10-20 15:52:49 +0800

    提交文档变动

--- src/components/Manage.vue
+++ src/components/Manage.vue
@@ -84,13 +84,24 @@ export default defineComponent({
     },
     commit: function () {
       let exec = require("child_process").exec;
+      exec("git branch", function (error, stdout, stderr) {
+        if (stdout) console.log(stdout);
+        if (error) console.error(stderr);
+      });
       exec("git add -A", function (error, stdout, stderr) {
         if (stdout) console.log(stdout);
         if (error) console.error(stderr);
       });
       exec("git commit -m '提交文档变动'", function (error, stdout, stderr) {
         if (stdout) console.log(stdout);
-        if (error) console.error(stderr);
+        if (error) {
+          console.error(stderr);
+        } else {
+          exec("git push", function (error, stdout, stderr) {
+            if (stdout) console.log(stdout);
+            if (error) console.error(stderr);
+          });
+        }
       });
     },
   },
