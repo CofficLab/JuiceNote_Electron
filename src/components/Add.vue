80749804a33776d2a68e52d1366e18ad261514e7
nookery <nooks@qq.com>
2022-11-18 11:13:55 +0800

    提交文档变动

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,22 +1,46 @@
 <template>
-  <button v-on:click="show" class="btn btn-sm my-auto rounded-none">
-    <Plus></Plus>
+  <button class="btn btn-sm my-auto rounded-none">
+    <div class="dropdown dropdown-top dropdown-hover">
+      <label tabindex="0"><Plus></Plus></label>
+      <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-neutral rounded-box w-52">
+        <li><a v-on:click="showChapterForm">添加章节</a></li>
+        <li><a v-on:click="showPageForm">添加页面</a></li>
+      </ul>
+    </div>
   </button>
 
   <!-- 增加章节的弹层 -->
-  <div class="modal z-50" v-bind:class="showModal ? 'modal-open' : ''">
+  <div class="modal z-50" v-bind:class="chapterFormSwitcher ? 'modal-open' : ''">
     <div class="modal-box">
       <input
         ref="title"
         type="text"
         autofocus
         v-model="title"
-        placeholder="输入标题"
+        placeholder="输入章节的标题"
         class="input input-bordered input-primary w-full max-w-xs"
       />
       <div class="modal-action">
-        <label for="my-modal" class="btn" v-on:click="hide">取消</label>
-        <label for="my-modal" class="btn" v-on:click="submit">提交</label>
+        <label for="my-modal" class="btn" v-on:click="hideChapterForm">取消</label>
+        <label for="my-modal" class="btn" v-on:click="submitChapterForm">提交</label>
+      </div>
+    </div>
+  </div>
+
+  <!-- 增加页面的弹层 -->
+  <div class="modal z-50" v-bind:class="pageFormSwitcher ? 'modal-open' : ''">
+    <div class="modal-box">
+      <input
+        ref="title"
+        type="text"
+        autofocus
+        v-model="title"
+        placeholder="输入页面的标题"
+        class="input input-bordered input-primary w-full max-w-xs"
+      />
+      <div class="modal-action">
+        <label for="my-modal" class="btn" v-on:click="hidePageForm">取消</label>
+        <label for="my-modal" class="btn" v-on:click="submitPageForm">提交</label>
       </div>
     </div>
   </div>
@@ -30,28 +54,48 @@ import Plus from "../icons/plus.vue";
 export default defineComponent({
   data() {
     return {
-      showModal: false,
+      pageFormSwitcher: false,
+      chapterFormSwitcher: false,
       title: "",
     };
   },
   methods: {
-    show() {
-      this.showModal = true;
+    showPageForm() {
+      this.pageFormSwitcher = true;
       this.$nextTick(function () {
         (this.$refs.title as any).focus();
       });
     },
-    hide() {
-      this.showModal = false;
+    showChapterForm() {
+      this.chapterFormSwitcher = true;
+      this.$nextTick(function () {
+        (this.$refs.title as any).focus();
+      });
     },
-    submit() {
+    hidePageForm() {
+      this.pageFormSwitcher = false;
+    },
+    hideChapterForm() {
+      this.chapterFormSwitcher = false;
+    },
+    submitPageForm() {
       let current = store.current;
       let parent = current.parent();
 
       if (parent.isEmpty()) return console.error("父节点不存在，无法创建");
 
       store.goto(store.createChild(parent, this.title).id);
-      this.showModal = false;
+      this.pageFormSwitcher = false;
+      this.title = "";
+    },
+    submitChapterForm() {
+      let current = store.current;
+      let parent = current.parent();
+
+      if (parent.isEmpty()) return console.error("父节点不存在，无法创建");
+
+      store.goto(store.createChild(parent, this.title).id);
+      this.chapterFormSwitcher = false;
       this.title = "";
     },
   },
