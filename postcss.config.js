6f77408269b369d51620ed122ee355d632f21c7a
nookery <nooks@qq.com>
2022-11-30 15:10:35 +0800

    提交文档变动

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: {
     "tailwindcss/nesting": {},
     tailwindcss: {},
-    autoprefixer: {},
+    // 桌面APP，css文件从本地加载，大也没关系，不需要autoprefixer
+    // autoprefixer: {},
   },
 };
