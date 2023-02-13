f34d1351a35192ccb39d87b04555ff3ee7c0da06
nooks <nooks@qq.com>
2023-02-13 23:23:53 +0800

    Update App.vue

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,7 +2,7 @@
   <div>
     <!-- 标题栏，左侧显示红绿灯，右侧可用于拖移 -->
     <div class="h-8 bg-stone-900 fixed top-0 z-50 w-full flex justify-center" id="title-bar" v-show="!hideTitleBar">
-      <!-- <Address v-if="!isProd"></Address> -->
+      <Address v-if="!isProd"></Address>
     </div>
 
     <main class="flex flex-row justify-between" v-bind:class="hideTitleBar ? 'mt-0' : 'mt-8'">
