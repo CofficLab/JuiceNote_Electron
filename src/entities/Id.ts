41007e46ce826e62f5bf9ee80ad7051c507d2b5f
nooks <nooks@qq.com>
2023-02-20 23:52:17 +0800

    新的ID规则

--- src/entities/Id.ts
+++ src/entities/Id.ts
@@ -5,25 +5,14 @@ import Config from "./Config"
 class Id {
     // 将文件路径转换成节点ID
     public static pathToId(path: string): string {
-        let id = path.replace(Config.markdownRootPath, '').replace('.md', '').replace('/', '').replaceAll('/', '@')
+        let id = path.replace(Config.markdownRootPath, '').replace('/', '').replaceAll('/', '@')
 
         return id === '' ? '/' : id
     }
 
     // 将节点ID转换成文件路径
     public static idToPath(id: string): string {
-        if (id == '') {
-            return Config.markdownRootPath
-        }
-
-        let folderPath = path.join(Config.markdownRootPath, id.replaceAll('@', '/'))
-        let markdownFilePath = folderPath + '.md'
-
-        if (fs.existsSync(markdownFilePath)) {
-            return markdownFilePath
-        }
-
-        return folderPath
+        return path.join(Config.markdownRootPath, id.replaceAll('@', '/'))
     }
 }
 
