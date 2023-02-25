ba0e0735c70255ef83b5fce8723c2497b2cf36ba
nooks <nooks@qq.com>
2023-02-25 14:12:07 +0800

    调整了代码高亮

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -13,7 +13,18 @@
 </template>
 
 <script lang="ts">
+import "prismjs/themes/prism.css";
+import "tui-color-picker/dist/tui-color-picker.css";
+import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
+import "@toast-ui/editor-plugin-table-merged-cell/dist/toastui-editor-plugin-table-merged-cell.css";
+import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css";
 import Prism from "prismjs";
+// 不导入这个，导入PHP会报错
+import "prismjs/components/prism-markup-templating.js";
+import "prismjs/components/prism-go.js";
+import "prismjs/components/prism-php.js";
+import "prismjs/components/prism-python.js";
+import "prismjs/components/prism-java.js";
 import { defineComponent } from "vue";
 import Editor from "@toast-ui/editor";
 import chart from "@toast-ui/editor-plugin-chart";
@@ -99,7 +110,6 @@ export default defineComponent({
         el: document.querySelector("#viewer") ?? document.createElement("div"),
         initialValue: RouteController.getCurrentPage().markdownSourceCode(),
         plugins: [
-          // colorSyntax,
           [codeSyntaxHighlight, { highlighter: Prism }],
           [chart, chartOptions],
         ],
