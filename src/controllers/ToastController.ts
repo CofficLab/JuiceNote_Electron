b614a9cf3be520678bbd48ebae06b06fae7d045f
nooks <nooks@qq.com>
2023-04-17 22:44:33 +0800

    提交文档变动

--- src/controllers/ToastController.ts
+++ src/controllers/ToastController.ts
@@ -1,10 +1,17 @@
 import { reactive } from 'vue'
 
+function truncate(str: string, maxLength: number): string {
+    if (str.length > maxLength) {
+        return str.slice(0, maxLength) + '...'
+    }
+    return str
+}
+
 const ToastController = reactive({
     toast: '',
 
     set(message: string): void {
-        this.toast = message
+        this.toast = truncate(message, 20)
 
         setTimeout(() => {
             console.log("清理toast")
