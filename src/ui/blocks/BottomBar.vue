c3fb26ae8418cf6005744c9bbd89c4e21543d116
nooks <nooks@qq.com>
2023-04-27 08:15:39 +0800

    Update BottomBar.vue

--- src/ui/blocks/BottomBar.vue
+++ src/ui/blocks/BottomBar.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="fixed bottom-0 left-40 w-full bg-yellow-500/70 px-4 shadow-2xl dark:bg-blue-500">
     <div class="flex flex-row items-center gap-12">
+      <Breadcrumbs :current="current" v-if="currentId > 0"></Breadcrumbs>
       <span class="my-auto text-red-700" v-html="url"></span>
       <span class="my-auto text-red-900" v-html="fullPath"></span>
     </div>
@@ -10,8 +11,12 @@
 <script setup>
 import { useRoute } from "vue-router";
 import { computed } from "vue";
+import Breadcrumbs from "./Breadcrumbs.vue";
+import Node from "../entities/Node";
 
 const route = useRoute();
 const url = computed(() => location.href);
 const fullPath = computed(() => route.fullPath);
+const currentId = computed(() => route.params.id);
+const current = computed(() => Node.find(currentId.value));
 </script>
