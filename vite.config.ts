c62bda228348887cda409cdcb31d26a0feb7bc90
nooks <nooks@qq.com>
2023-05-01 23:00:51 +0800

    修复打包问题

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ rmSync('dist', { recursive: true, force: true })
 
 export default defineConfig({
   build: {
-    outDir: '../../dist/ui',
+    outDir: 'dist/ui',
   },
   base: './',
   root: './',
