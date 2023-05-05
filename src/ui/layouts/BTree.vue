02b862675f3a017892b55b3da0b30fe72f543a3c
nookery <nooks@qq.com>
2023-05-05 18:46:22 +0800

    footer使用tree

--- src/ui/layouts/BTree.vue
+++ src/ui/layouts/BTree.vue
@@ -3,15 +3,17 @@
     <div :class="{
       'flex': true,
       'flex-col': display == 'col',
-      'flex-row p-1': display == 'row',
+      'flex-row': display == 'row',
+      'p-1':display=='row' && !activeOnesShowOnly
     }">
       <div v-show="shouldShow" @mouseenter="hoverCallback(tree)" :class="{
         'flex flex-row items-center p-0 text-xs': true,
         'hover:bg-primary-focus/20': !shouldActive() || tree.isDatabase,
-        'bg-primary text-primary-content': shouldActive() && (tree.isPage || tree.isTab),
+        'bg-primary text-primary-content': shouldActive() && (tree.isPage || tree.isTab) && !activeOnesShowOnly,
         'bg-primary/5': shouldActive() && tree.isChapter && !tree.isTab,
         'w-48': display == 'row',
-        'border-l border-t border-b border-accent-content': display == 'row' && open,
+        'border-l border-t border-b border-accent-content': display == 'row' && open && !activeOnesShowOnly,
+        'border-0':activeOnesShowOnly
       }">
         <Link :node="tree" @click="setOpen" @mouseenter="hoverCallback(tree)" :class="{
           'flex flex-grow cursor-pointer flex-row items-center gap-2 px-2 py-2': true,
@@ -27,7 +29,7 @@
         </Link>
 
         <!-- 折叠按钮 -->
-        <div @click="toggle" v-if="(tree.isChapter) || tree.isBook" class="btn-ghost rounded-none btn-square btn-sm btn">
+        <div @click="toggle" v-if="(tree.isChapter || tree.isBook) && !activeOnesShowOnly" class="btn-ghost rounded-none btn-square btn-sm btn">
           {{
             open ? "-" : "+" }}</div>
       </div>
@@ -36,9 +38,10 @@
       <div :class="{
         'flex flex-col rounded-none pl-2': true,
         'border border-accent-content gap-0 border-l-0': display == 'row',
-      }" v-if="(tree.getChildren().length > 0 && open) || shouldHide">
-        <BTree v-for="child in tree.getChildren()" :display="display" :tree="child" :hover-callback="hoverCallback"
-          :current-node="currentNode"></BTree>
+        'border-0':activeOnesShowOnly
+      }" v-if="shouldChildrenShow">
+        <BTree v-for="child in tree.getChildren()" :active-ones-show-only="activeOnesShowOnly" :display="display"
+          :tree="child" :hover-callback="hoverCallback" :current-node="currentNode"></BTree>
       </div>
     </div>
   </div>
@@ -79,15 +82,32 @@ const props = defineProps({
     default: (node: Node) => {
       console.log('tree hovered', node.title)
     },
+  },
+  activeOnesShowOnly: {
+    type: Boolean,
+    default: false,
   }
 });
 
 const shouldShow = computed(() => {
+  if (props.activeOnesShowOnly) {
+    return shouldActive()
+  }
+
   return !props.hiddenList.includes(props.tree.id) && props.tree.getParents().length < props.depth
+})
+
+const shouldChildrenShow = computed(() => {
+  if (props.activeOnesShowOnly && props.display == 'row') {
+    return shouldActive()
+  }
+
+  return (props.tree.getChildren().length > 0 && open.value) || shouldHide.value
 })
 
 const shouldActive = (node?: Node) => {
   if (node == undefined) node = props.tree
+  if (node.isDatabase) return true
 
   return node.id == props.currentNode.id || props.currentNode.getParents().some((parent) => parent.id == node!.id);
 };
--- src/ui/layouts/Footer.vue
+++ src/ui/layouts/Footer.vue
@@ -1,6 +1,7 @@
 <template>
   <footer>
-    <Breadcrumbs :current="current"></Breadcrumbs>
+    <!-- <Breadcrumbs :current="current"></Breadcrumbs> -->
+    <BTree :tree="DatabaseNode" :display="'row'" :current-node="current" :active-ones-show-only="true"></BTree>
   </footer>
 </template>
 
@@ -8,6 +9,8 @@
 import { computed } from "vue";
 import Breadcrumbs from "./Breadcrumbs.vue";
 import { useCurrentNodeStore } from "../stores/NodeStore";
+import BTree from "./BTree.vue";
+import { DatabaseNode } from "../entities/Node";
 
 const current = computed(() => useCurrentNodeStore().current);
 </script>
