6830e2034c93a7f5a7477a5cbf52ef1d91035725
nookery <nooks@qq.com>
2023-05-31 13:55:07 +0800

    Update App.vue

--- src/ui/layouts/App.vue
+++ src/ui/layouts/App.vue
@@ -21,7 +21,7 @@
         </transition>
       </router-view>
 
-      <Footer v-if="!route.name?.toString().startsWith('setting')" class="fixed w-full bottom-0 z-40 h-8 bg-primary/60 px-2 shadow-2xl backdrop-blur backdrop-filter"></Footer>
+      <Footer v-if="!route.name?.toString().startsWith('setting')" class="fixed w-full bottom-0 z-40 h-8 bg-gradient-to-r from-primary/50 to-primary/60 px-2 shadow-2xl backdrop-blur backdrop-filter"></Footer>
     </main>
 
     <!-- 弹层 -->
